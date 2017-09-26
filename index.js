657116a98f8d6db6904f9639347e71b2cc2ee4f4
BridledHawk <isaacbaiano316@gmail.com>
2017-09-26 17:05:03 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ bot.on('message', message => {
                 message.channel.send("Joining your voice channel.");
                 play(connection, message)
             });
-            message.channel.send('Song added to queue!');
+            message.channel.send( `Song added to queue! ${server.queue.length} songs in queue.`);
             var server = servers[message.guild.id];
 
             server.queue.push(args[1]);
